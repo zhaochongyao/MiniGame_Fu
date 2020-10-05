6b8b2c01120539ab106cf08d63ac89a54d701f7b
zhaochongyao <59955881+zhaochongyao@users.noreply.github.com>
2020-10-05 15:06:45 +0800

    Create MoveController.cs

---
+++ Fu/Assets/Scripts/MoveController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Windows版本的主角控制器
+/// </summary>
+public class MoveController : MonoBehaviour
+{
+    /// <summary>
+    /// 主角移动脚本
+    /// </summary>
+    PlayerMove playerMove;          
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMove = GetComponent<PlayerMove>(); 
+    }
+    /// <summary>
+    /// 接收输入信号
+    /// </summary>
+    void CheckInput()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        playerMove.walk(x,y);
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            playerMove.jump();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerMove.crouch();
+        }
+        else 
+        {
+            playerMove.overCrouch();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+    }
+}
